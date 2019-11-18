85e6e95638d98b4b161ec7cbe841a0b0ca11623e
Ahmed Bouhram <ahmedbohram86@gmail.com>
2019-11-18 07:48:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener('keypress',function(e){
+    console.log(e);
+  
+
+}); 
+
+const myCustomDiv = document.createElement('div');
+
+function respondToTheClick(evt) {
+    console.log('A paragraph was clicked: ' + evt.target.textContent);
+}
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is paragraph number ' + i;
+
+    myCustomDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myCustomDiv);
+
+myCustomDiv.addEventListener('click', respondToTheClick);
